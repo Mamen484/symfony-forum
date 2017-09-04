f92d6d82e0f9d8e44378f0bec4e7a8c3f1454a5a
Mamen484 <30903165+Mamen484@users.noreply.github.com>
2017-09-04 10:02:09 +0200

    Liens vers le détails des post en utilisant le slug

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -3,37 +3,56 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\AppBundle;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Gedmo\Mapping\Annotation\Slug;
 
 class PostController extends Controller
 {
 
     /**
-     * @param $id
-     * @Route("/post/{id}",
-     *          name="post_details",
-     *          requirements={"id":"\d+"}
+     * @param $slug
+     * @Route("/post/{slug}",
+     *          name="post_details"
      * )
      * @return Response
      */
-    public function detailsAction($id){
+    public function detailsAction($slug)
+    {
 
         $repository = $this->getDoctrine()
             ->getRepository("AppBundle:Post");
+        /** @var $post Post */
+        $post = $repository->findOneBySlug($slug);
 
-        $post = $repository->find($id);
-
-        if(! $post){
+        if (!$post) {
             throw new NotFoundHttpException("post introuvable");
         }
 
         return $this->render("post/details.html.twig", [
             "post" => $post,
             "answerList" => $post->getAnswers()
+        ]);
+    }
+
+    /**
+     * $Route("post-par-annee/{year}", name="post_by_year")
+     *      requirements={"year":"\d{4}"})
+     * @param $year
+     * @return Response
+     */
+    public function postByYearAction($year)
+    {
+        $postRespository = $this->getDoctrine()
+            ->getRepository("AppBundle:Post");
+
+        return $this->render("default/theme.html.twig", [
+            "title" => "Liste des posts par année ({$year})",
+            "postList" => $postRespository->getPostsByYear($year)
         ]);
     }
 
